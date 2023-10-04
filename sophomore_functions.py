f5de0c94a3a8ff20fa8903f952f547c5fec7d546
hieuminh65 <hieuminhnguyen@usf.edu>
2023-10-04 13:41:14 -0400

    add functions

--- sophomore_functions.py
+++ sophomore_functions.py
@@ -1,8 +1,9 @@
 import json
-def delete_data(data_file, keywords):
+def delete_data(data_file):
     """Function use to delete data based on keywords"""
     """data_file[str]: path to the data file. Example: 'data.json'"""
     """keywords[list]: list of keywords to delete the data. Example: ['youtube', 'instagram']"""
+    keywords = ['youtube', 'instagram']
     with open(data_file) as f:
         data = json.load(f)
     for item in data:
@@ -12,6 +13,5 @@ def delete_data(data_file, keywords):
     return data
 
 # Usage:
-keywords = ['youtube', 'instagram']
 data_file = 'data.json'
-data = delete_data(data_file, keywords)
+data = delete_data(data_file)
