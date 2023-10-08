695820d590d3a145b4a54cc6fbec84e60988d8d4
QuanMichaelNguyen <144957908+QuanMichaelNguyen@users.noreply.github.com>
2023-10-08 10:39:51 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,21 +8,20 @@ def filter(data_file, keywords_dining):
             filter_data.append(item['metadata']['source'])
     return filter_data
 
-# method used for deleting \n
+#Method for deleteing \n
 data_file = 'data.json'
 def delete_newlines(data_file):
-    new_data = []
-    data = json.load(open(data_file))
-    for item in data:
-        new_page_content = item['page_content'].replace('\n', '')
-
-        modified_item = {
-            'page_content': new_page_content,
-        }
-
-        new_data.append(modified_item)
-
-    return new_data
+   new_data = []
+   data = json.load(open(data_file)) # as a way to open the data.json file
+   for item in data:
+       new_page_content = (item['page_content'].replace('\n', '') for data_file in data_file)
+       modified_item = { # so now modified_item is the dict of data
+           'page_content': new_page_content,
+           'metadata': 'page_content'
+       }
+       new_data.append(modified_item) # new_data is a new version of data without \n
+   return new_data
+# so the main mission is finding a way to match new type of variable to the type of old variable
 
 
 
